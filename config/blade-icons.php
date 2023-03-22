2c2d2fa11bea5cf07c9246f28abc859b39eab57e
collinberg <berg.collin@gmail.com>
2023-03-22 10:58:29 -0400

    Config Files

---
+++ config/blade-icons.php
@@ -0,0 +1,11 @@
+<?php
+
+
+return [
+    'sets' => [
+        'default' => [
+            'path' => 'resources/images/icons', # Relative path to the new directory
+            'prefix' => 'icon',
+        ],
+    ],
+];
